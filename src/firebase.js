4e25ca197ce13b5e0b005bb8ad40f3464cf8f5ef
sohibjonabdialimov <sohibjonabdialimov6@gmail.com>
2024-04-19 16:38:41 +0500

    change

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: "shop-91d46.firebaseapp.com",
+  projectId: "shop-91d46",
+  storageBucket: "shop-91d46.appspot.com",
+  messagingSenderId: "355944258190",
+  appId: "1:355944258190:web:bb4e188d06151e746187df",
+};
+
+// console.log(process.env.REACT_APP_FIREBASE_KEY);
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth();
+export const storage = getStorage(app);
